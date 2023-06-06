b9927c003d16a76190a24b3c3f67bef44cdee140
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-06-06 09:20:08 -0300

    Revert "Removendo artefatos da estória seguinte"
    This reverts commit 0a72cc2006831884a921d0076021e24a740fd739.

--- SME.CDEP.IoC/RegistradorDeDependencia.cs
+++ SME.CDEP.IoC/RegistradorDeDependencia.cs
@@ -1,8 +1,16 @@
 ﻿using Dapper.FluentMap;
 using Dapper.FluentMap.Dommel;
+using Elastic.Apm.Api;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SME.CDEP.Aplicacao.Servicos;
+using SME.CDEP.Aplicacao.Servicos.Interface;
+using SME.CDEP.Dominio.Dominios;
 using SME.CDEP.Infra.Dados;
+using SME.CDEP.Infra.Dados.Mapeamentos;
+using SME.CDEP.Infra.Dados.Repositorios;
+using SME.CDEP.Infra.Dados.Repositorios.Interfaces;
 using SME.CDEP.Infra.Servicos.Polly;
 using SME.CDEP.Infra.Servicos.Telemetria.IoC;
 
@@ -28,10 +36,12 @@ public class RegistradorDeDependencia
         RegistrarServicos();
     }
 
-    protected virtual void RegistrarMapeamentos()
+    private void RegistrarMapeamentos()
     {
         FluentMapper.Initialize(config =>
         {
+            config.AddMap(new UsuarioMap());
+
             config.ForDommel();
         });
     }
@@ -43,7 +53,13 @@ public class RegistradorDeDependencia
 
     protected virtual void RegistrarConexao()
     {
-        _serviceCollection.AddScoped<ICdepConexao, CdepConexao>(_ => new CdepConexao(_configuration.GetConnectionString("conexao")));
+        //TODO: Rever isso
+        var conexao = _configuration.GetConnectionString("conexao");
+        if (conexao != null)
+            _serviceCollection.AddScoped<ICdepConexao, CdepConexao>(_ => new CdepConexao(conexao));
+        else
+            _serviceCollection.AddScoped<ICdepConexao, CdepConexao>();    
+        
         _serviceCollection.AddScoped<ITransacao, Transacao>();
     }
 
@@ -53,8 +69,12 @@ public class RegistradorDeDependencia
     }
 
     protected virtual void RegistrarRepositorios()
-    {}
+    {
+        _serviceCollection.TryAddScoped<IRepositorioUsuario, RepositorioUsuario>();
+    }
 
     protected virtual void RegistrarServicos()
-    {}
+    {
+        _serviceCollection.TryAddScoped<IServicoUsuario, ServicoUsuario>();
+    }
 }
--- SME.CDEP.TesteIntegracao/Setup/RegistradorDependencias.cs
+++ SME.CDEP.TesteIntegracao/Setup/RegistradorDependencias.cs
@@ -1,64 +1,14 @@
-﻿using Dapper.FluentMap;
-using Dapper.FluentMap.Dommel;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SME.CDEP.Infra.Dados;
-using SME.CDEP.Infra.Servicos.Polly;
-using SME.CDEP.Infra.Servicos.Telemetria.IoC;
 using SME.CDEP.IoC;
 
 namespace SME.CDEP.TesteIntegracao.Setup
 {
     public class RegistradorDependencias : RegistradorDeDependencia
     {
-        private readonly IServiceCollection _serviceCollection;
-        private readonly IConfiguration _configuration;
-        
         public RegistradorDependencias(IServiceCollection serviceCollection, IConfiguration configuration) : base(serviceCollection, configuration)
         {
-            _serviceCollection = serviceCollection;
-            _configuration = configuration;
+           
         }
-        
-        public override void Registrar()
-        {
-            RegistrarTelemetria();
-            RegistrarConexao();
-            RegistrarRepositorios();
-            RegistrarPolly();
-            RegistrarMapeamentos();
-            RegistrarServicos();
-        }
-
-        protected override void RegistrarConexao()
-        {
-            _serviceCollection.AddScoped<ICdepConexao, CdepConexao>();
-            _serviceCollection.AddScoped<ITransacao, Transacao>();
-        }
-        
-        protected override void RegistrarMapeamentos()
-        {
-            FluentMapper.Initialize(config =>
-            {
-                config.ForDommel();
-            });
-        }
-
-        protected override void RegistrarTelemetria()
-        {
-            _serviceCollection.ConfigurarTelemetria(_configuration);
-        }
-        
-        protected override void RegistrarPolly()
-        {
-            _serviceCollection.ConfigurarPolly();
-        }
-
-        protected override void RegistrarRepositorios()
-        {}
-
-        protected override void RegistrarServicos()
-        {}
-        
     }
 }
