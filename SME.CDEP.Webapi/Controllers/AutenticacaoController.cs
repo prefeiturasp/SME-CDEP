9392e0b3248fb1db806335800dbaa8949e09d254
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-06-07 12:03:02 -0300

    Criação do endopint de autenticação

---
+++ SME.CDEP.Webapi/Controllers/AutenticacaoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SME.CDEP.Webapi.Controllers;
+
+[ApiController]
+[Route("api/v1/autenticacao")]
+[ValidaDto]
+[Authorize("Bearer")]
+public class AutenticacaoController
+{
+    
+}
