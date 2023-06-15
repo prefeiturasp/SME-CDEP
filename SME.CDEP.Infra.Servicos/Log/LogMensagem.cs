a5e45f8a119796a40d4ec3967772a561819a8b84
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-06-15 16:20:15 -0300

    Arquivos não foram adicionados

---
+++ SME.CDEP.Infra.Servicos/Log/LogMensagem.cs
@@ -0,0 +1,22 @@
+﻿namespace SME.CDEP.Infra.Servicos.Log
+{
+    public class LogMensagem
+    {
+        public LogMensagem(string mensagem, string contexto = "", string nivel = "", string observacao = "", string rastreamento = "", string projeto = "Acessos")
+        {
+            Mensagem = mensagem;
+            Contexto = contexto;
+            Nivel = nivel;
+            Observacao = observacao;
+            Rastreamento = rastreamento;
+            Projeto = projeto;
+        }
+
+        public string Mensagem { get; }
+        public string Contexto { get; }
+        public string Nivel { get; }
+        public string Observacao { get; }
+        public string Rastreamento { get; }
+        public string Projeto { get; }
+    }
+}
