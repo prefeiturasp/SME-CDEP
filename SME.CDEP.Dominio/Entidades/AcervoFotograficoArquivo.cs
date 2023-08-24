ec5e6b0ce4a5ee7082faad4e62f252bbad1376fa
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-08-24 17:50:56 -0300

    Criar mapeamentos e entidades

---
+++ SME.CDEP.Dominio/Entidades/AcervoFotograficoArquivo.cs
@@ -0,0 +1,9 @@
+﻿namespace SME.CDEP.Dominio.Entidades
+{
+    public class AcervoFotograficoArquivo
+    {
+        public long Id { get; set; }
+        public long AcervoFotograficoId { get; set; }
+        public long ArquivoId { get; set; }
+    }
+}
---
+++ SME.CDEP.Infra.Dados/Mapeamentos/AcervoFotograficoMap.cs
@@ -0,0 +1,30 @@
+﻿using SME.CDEP.Dominio.Entidades;
+
+namespace SME.CDEP.Infra.Dados.Mapeamentos
+{
+    public class AcervoFotograficoMap : BaseMapAuditavel<AcervoFotografico>
+    {
+        public AcervoFotograficoMap()
+        {
+            ToTable("acervo_fotografico");
+            Map(c => c.Titulo).ToColumn("titulo");
+            Map(c => c.CreditoId).ToColumn("credito_id");
+            Map(c => c.Tombo).ToColumn("tombo");
+            Map(c => c.Localizacao).ToColumn("localizacao");
+            Map(c => c.Procedencia).ToColumn("procedencia");
+            Map(c => c.DataAcervo).ToColumn("data_acervo");
+            Map(c => c.CopiaDigital).ToColumn("copia_digital");
+            Map(c => c.PermiteUsoImagem).ToColumn("permite_uso_imagem");
+            Map(c => c.ConservacaoId).ToColumn("conservacao_id");
+            Map(c => c.Descricao).ToColumn("descricao");
+            Map(c => c.Quantidade).ToColumn("quantidade");
+            Map(c => c.Largura).ToColumn("largura");
+            Map(c => c.Altura).ToColumn("altura");
+            Map(c => c.SuporteId).ToColumn("suporte_id");
+            Map(c => c.Formato_id).ToColumn("formato_id");
+            Map(c => c.Cromia_id).ToColumn("cromia_id");
+            Map(c => c.Resolucao).ToColumn("resolucao");
+            Map(c => c.TamanhoArquivo).ToColumn("tamanho_arquivo");
+        }
+    }
+}
