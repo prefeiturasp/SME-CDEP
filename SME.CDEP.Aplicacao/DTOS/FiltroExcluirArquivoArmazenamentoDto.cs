ddaac24f7a9ae0d60ab3f8c28618dcfdba1038f4
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-09-08 17:04:03 -0300

    Ajustando a mensagem de exclusão de arquivos no bucket

---
+++ SME.CDEP.Aplicacao/DTOS/FiltroExcluirArquivoArmazenamentoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SME.CDEP.Aplicacao.DTOS
+{
+    public class FiltroExcluirArquivoArmazenamentoDto
+    {
+        public string ArquivoNome { get; set; }
+        public string BucketNome { get; set; }
+    }
+}
