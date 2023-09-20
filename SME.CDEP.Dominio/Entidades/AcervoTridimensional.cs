d0e59c13088eb10649fec73ce4dad4a8d81cc1f4
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-09-20 16:32:55 -0300

    Criar mapeamentos e entidades - tridimensional

---
+++ SME.CDEP.Dominio/Entidades/AcervoTridimensional.cs
@@ -0,0 +1,17 @@
+﻿namespace SME.CDEP.Dominio.Entidades
+{
+    public class AcervoTridimensional : EntidadeBase
+    {
+        public Acervo Acervo { get; set; }
+        public long AcervoId { get; set; }
+        public string Procedencia { get; set; }
+        public string DataAcervo { get; set; }
+        public long ConservacaoId { get; set; }
+        public long Quantidade { get; set; }
+        public string Descricao { get; set; }
+        public float Largura { get; set; }
+        public float Altura { get; set; }
+        public float Profundidade { get; set; }
+        public float Diametro { get; set; }
+    }
+}
