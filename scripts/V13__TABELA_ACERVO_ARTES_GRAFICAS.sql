d3e1c861f64407515ad8638a6c18646bbe27b39a
Vinícius Adelir Nyari <47329467+viniciusnyari@users.noreply.github.com>
2023-09-20 08:43:58 -0300

    Update V13__TABELA_ACERVO_ARTES_GRAFICAS.sql
    Removendo o plural

--- scripts/V13__TABELA_ACERVO_ARTES_GRAFICAS.sql
+++ scripts/V13__TABELA_ACERVO_ARTES_GRAFICAS.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE if not exists public.acervo_artes_graficas (
+﻿CREATE TABLE if not exists public.acervo_arte_grafica (
 	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY( NO MINVALUE NO MAXVALUE NO CYCLE),
 	acervo_id int8 not null,
 	localizacao varchar(100) NULL,
@@ -15,15 +15,15 @@
 	suporte_id int8 NOT NULL,
 	quantidade int4 NULL,	
 	descricao text NULL,
-	CONSTRAINT acervo_artes_graficas_pk PRIMARY KEY (id),
-	CONSTRAINT acervo_artes_graficas_acervo_fk FOREIGN KEY (acervo_id) REFERENCES public.acervo(id)
+	CONSTRAINT acervo_arte_grafica_pk PRIMARY KEY (id),
+	CONSTRAINT acervo_arte_grafica_acervo_fk FOREIGN KEY (acervo_id) REFERENCES public.acervo(id)
 );
 
-CREATE TABLE if not exists public.acervo_artes_graficas_arquivo (
+CREATE TABLE if not exists public.acervo_arte_grafica_arquivo (
 	id int8 NOT NULL GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE),
-	acervo_artes_graficas_id int8 NOT NULL,
+	acervo_arte_grafica_id int8 NOT NULL,
 	arquivo_id int8 NOT NULL,
-	CONSTRAINT acervo_artes_graficas_arquivo_pk PRIMARY KEY (id),
-	CONSTRAINT acervo_artes_graficas_arquivo_arquivo_fk FOREIGN KEY (arquivo_id) REFERENCES public.arquivo(id),
-	CONSTRAINT acervo_artes_graficas_arquivo_acervo_artes_graficas_fk FOREIGN KEY (acervo_artes_graficas_id) REFERENCES public.acervo_artes_graficas(id)
+	CONSTRAINT acervo_arte_grafica_arquivo_pk PRIMARY KEY (id),
+	CONSTRAINT acervo_arte_grafica_arquivo_arquivo_fk FOREIGN KEY (arquivo_id) REFERENCES public.arquivo(id),
+	CONSTRAINT acervo_arte_grafica_arquivo_acervo_arte_grafica_fk FOREIGN KEY (acervo_arte_grafica_id) REFERENCES public.acervo_arte_grafica(id)
 );
