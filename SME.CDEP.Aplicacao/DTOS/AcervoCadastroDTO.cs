a57218d01788e327f7e218088727b9cbec4ba15f
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-09-26 15:55:17 -0300

    Ajustando o nome do campo

--- SME.CDEP.Aplicacao/DTOS/AcervoCadastroDTO.cs
+++ SME.CDEP.Aplicacao/DTOS/AcervoCadastroDTO.cs
@@ -4,14 +4,14 @@ namespace SME.CDEP.Aplicacao.DTOS;
 
 public class AcervoCadastroDTO
 {
-    [Required(ErrorMessage = "É necessário informar o título do acervo fotográfico")]
-    [MaxLength(500, ErrorMessage = "A localização do acervo fotográfico não pode conter mais que 500 caracteres")]
+    [Required(ErrorMessage = "É necessário informar o título do acervo")]
+    [MaxLength(500, ErrorMessage = "A localização do acervo não pode conter mais que 500 caracteres")]
     public string Titulo { get; set; }
     
-    [Required(ErrorMessage = "É necessário informar o Tombo do acervo fotográfico")]
+    [Required(ErrorMessage = "É necessário informar o Tombo do acervo")]
     [MaxLength(12, ErrorMessage = "O Tombo do acervo fotográfico não pode conter mais que 12 caracteres")]
     public string Codigo { get; set; }
     
-    [Required(ErrorMessage = "É necessário informar ao menos um crédito ou autoria do acervo fotográfico")]
+    [Required(ErrorMessage = "É necessário informar ao menos um crédito ou autoria do acervo")]
     public long[] CreditosAutoresIds { get; set; }
 }
