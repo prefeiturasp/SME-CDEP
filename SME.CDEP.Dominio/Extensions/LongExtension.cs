8ed3c372990385cb7a567015b8442c37a3c63769
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-10-23 08:47:41 -0300

    Ajustar a mensagem de crítica do campo antigo

---
+++ SME.CDEP.Dominio/Extensions/LongExtension.cs
@@ -0,0 +1,13 @@
+﻿
+using SME.CDEP.Infra.Dominio.Enumerados;
+
+namespace SME.CDEP.Dominio.Extensions
+{
+    public static class LongExtension
+    {
+        public static bool EhAcervoDocumental(this long valor)
+        {
+            return valor == (int)TipoAcervo.DocumentacaoHistorica; 
+        }
+    }
+}
