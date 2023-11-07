2597bc48c051a88d634a101e54216f3fdfdd802e
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-11-07 07:47:52 -0300

    Corrigindo nome do método - bibliografico

--- SME.CDEP.Webapi/Controllers/ImportacaoArquivoAcervoBibliograficoController.cs
+++ SME.CDEP.Webapi/Controllers/ImportacaoArquivoAcervoBibliograficoController.cs
@@ -16,8 +16,8 @@ public class ImportacaoArquivoAcervoBibliograficoController: BaseController
     [ProducesResponseType(200)]
     [ProducesResponseType(401)]
     [ProducesResponseType(typeof(RetornoBaseDTO), 500)]
-    [Authorize("Bearer")]
-    public async Task<IActionResult> UploadPorTipoAcervo(IFormFile file,[FromServices] IServicoImportacaoArquivoAcervoBibliografico servicoImportacaoArquivoAcervoBibliografico)
+    [Authorize("Bearer")] 
+    public async Task<IActionResult> ImportarArquivo(IFormFile file,[FromServices] IServicoImportacaoArquivoAcervoBibliografico servicoImportacaoArquivoAcervoBibliografico)
     {
         return Ok(await servicoImportacaoArquivoAcervoBibliografico.ImportarArquivo(file));
     }
