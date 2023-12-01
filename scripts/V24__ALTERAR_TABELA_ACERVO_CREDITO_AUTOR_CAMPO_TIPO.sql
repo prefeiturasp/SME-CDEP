312684f1ca19c5495a4fa7e8241682b13d0350a5
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-12-01 16:49:56 -0300

    Adicionando if not exists

--- scripts/V24__ALTERAR_TABELA_ACERVO_CREDITO_AUTOR_CAMPO_TIPO.sql
+++ scripts/V24__ALTERAR_TABELA_ACERVO_CREDITO_AUTOR_CAMPO_TIPO.sql
@@ -1,2 +1,2 @@
 ﻿--> Ajustando campo tipo na tabela de autor
-alter table acervo_credito_autor add column ehCoAutor bool default(false);
+alter table acervo_credito_autor add column IF NOT EXISTS ehCoAutor bool default(false);
