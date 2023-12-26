67158bed097672c1dbd9520e7668dc80b7bcf5be
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2023-12-26 09:08:21 -0300

    Ajuste de endereço de arquivo miniatura

--- SME.CDEP.Dominio/Entidades/AcervoCodigoNomeResumido.cs
+++ SME.CDEP.Dominio/Entidades/AcervoCodigoNomeResumido.cs
@@ -1,19 +1,8 @@
-﻿using SME.CDEP.Infra.Dominio.Enumerados;
-
-namespace SME.CDEP.Dominio.Entidades
+﻿namespace SME.CDEP.Dominio.Entidades
 {
     public class AcervoCodigoNomeResumido 
     {
         public long AcervoId { get; set; }
-        public Guid Codigo { get; set; }
         public string Nome { get; set; }
-
-        public string NomeArquivo
-        {
-            get
-            {
-                return $"{Codigo}.{Nome.Split('.').LastOrDefault()}";
-            }
-        }
     }
 }
