ef2f082070c708c1ed08446a1fdf78061e91ee83
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-01-23 16:35:40 -0300

    Ajustando o retorno do Post para long

--- SME.CDEP.Aplicacao/Servicos/Interface/IServicoAcervoSolicitacao.cs
+++ SME.CDEP.Aplicacao/Servicos/Interface/IServicoAcervoSolicitacao.cs
@@ -4,7 +4,7 @@ namespace SME.CDEP.Aplicacao.Servicos.Interface
 {
     public interface IServicoAcervoSolicitacao : IServicoAplicacao
     {
-        Task<IEnumerable<AcervoSolicitacaoItemRetornoCadastroDTO>> Inserir(AcervoSolicitacaoItemCadastroDTO[] acervosSolicitacaoItensCadastroDTO);
+        Task<long> Inserir(AcervoSolicitacaoItemCadastroDTO[] acervosSolicitacaoItensCadastroDTO);
         Task<IEnumerable<AcervoSolicitacaoItemRetornoCadastroDTO>> ObterPorId(long acervoSolicitacaoId);
         Task<bool> Remover(long acervoSolicitacaoId);
         Task<IEnumerable<AcervoTipoTituloAcervoIdCreditosAutoresDTO>> ObterItensDoAcervoPorFiltros(long[] acervosIds);
