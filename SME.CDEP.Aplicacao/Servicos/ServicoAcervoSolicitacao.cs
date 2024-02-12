36606aba994620acbc4f36e99ba36c0970fc2379
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-02-12 08:58:35 -0300

    Criando um endpoint para cancelar mais de um item de acervo por vez

--- SME.CDEP.Aplicacao/Servicos/ServicoAcervoSolicitacao.cs
+++ SME.CDEP.Aplicacao/Servicos/ServicoAcervoSolicitacao.cs
@@ -340,5 +340,15 @@ namespace SME.CDEP.Aplicacao.Servicos
             
             return true;
         }
+
+        public async Task<bool> CancelarItensAcervoSolicitacao(long[] acervoSolicitacaoItemIds)
+        {
+            var retornoCancelamento = false;
+            
+            foreach (var item in acervoSolicitacaoItemIds)
+                retornoCancelamento = await CancelarItemAtendimento(item);
+
+            return retornoCancelamento;
+        }
     }
 }
