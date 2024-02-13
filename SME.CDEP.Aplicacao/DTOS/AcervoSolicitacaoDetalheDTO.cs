fd69136f49999781301d0422b6893721691ae06d
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-02-13 11:48:17 -0300

    Retornando o id da situação para habilitar botões de ações

--- SME.CDEP.Aplicacao/DTOS/AcervoSolicitacaoDetalheDTO.cs
+++ SME.CDEP.Aplicacao/DTOS/AcervoSolicitacaoDetalheDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace SME.CDEP.Aplicacao.DTOS
+﻿using SME.CDEP.Infra.Dominio.Enumerados;
+
+namespace SME.CDEP.Aplicacao.DTOS
 {
     public class AcervoSolicitacaoDetalheDTO
     {
@@ -9,6 +11,7 @@
         public DateTime DataSolicitacao { get; set; }
         public string ResponsavelRf { get; set; }
         public string Situacao { get; set; }
+        public SituacaoSolicitacao SituacaoId { get; set; }
         public IEnumerable<AcervoSolicitacaoItemDetalheResumidoDTO> Itens { get; set; } = Enumerable.Empty<AcervoSolicitacaoItemDetalheResumidoDTO>();
     }
 }
