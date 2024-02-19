a7e4077eab4e606a6997b1c79f15c88caddf08de
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-02-19 13:24:38 -0300

    Criando controllers de gestão de visita

---
+++ SME.CDEP.Webapi/Controllers/EventoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.CDEP.Aplicacao.DTOS;
+using SME.CDEP.Aplicacao.Servicos.Interface;
+using SME.CDEP.Infra.Dominio.Enumerados;
+using SME.CDEP.Webapi.Filtros;
+
+namespace SME.CDEP.Webapi.Controllers;
+
+[ApiController]
+[ValidaDto]
+public class EventoController: BaseController
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(long), 200)]
+    [ProducesResponseType(typeof(RetornoBaseDTO), 400)]
+    [ProducesResponseType(typeof(RetornoBaseDTO), 403)]
+    [ProducesResponseType(typeof(RetornoBaseDTO), 422)]
+    [ProducesResponseType(typeof(RetornoBaseDTO), 500)]
+    [ProducesResponseType(typeof(RetornoBaseDTO), 601)]
+    [Permissao(Permissao., Policy = "Bearer")]
+    public async Task<IActionResult> Inserir([FromBody] AcervoSolicitacaoItemCadastroDTO[] acervosSolicitacaoItensCadastroDTO, [FromServices] IServicoAcervoSolicitacao servicoAcervoSolicitacao)
+    {
+        return Ok(await servicoAcervoSolicitacao.Inserir(acervosSolicitacaoItensCadastroDTO));
+    }
+}
