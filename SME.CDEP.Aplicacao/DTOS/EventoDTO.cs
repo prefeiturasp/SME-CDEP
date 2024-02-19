c7262e9671a2b09374abb707e46876b12c459975
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-02-19 13:13:57 -0300

    Corrigindo campo nãio nulo

--- SME.CDEP.Aplicacao/DTOS/EventoDTO.cs
+++ SME.CDEP.Aplicacao/DTOS/EventoDTO.cs
@@ -5,7 +5,7 @@ namespace SME.CDEP.Aplicacao.DTOS
     public class EventoDTO
     {
         public long Id { get; set; }
-        public DateTime? Data { get; set; }
+        public DateTime Data { get; set; }
         public TipoEvento Tipo { get; set; }
         public string Descricao { get; set; }
         public string Justificativa { get; set; }
