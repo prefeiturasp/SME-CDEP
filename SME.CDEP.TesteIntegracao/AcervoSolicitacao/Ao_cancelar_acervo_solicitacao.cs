a706c5ffa7314d3cf20efac0701f393587340e01
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-02-21 14:22:31 -0300

    Permite cancelamento de atendimento em todas as situações, exceto quando itens em finalizado automaticamente

--- SME.CDEP.TesteIntegracao/AcervoSolicitacao/Ao_cancelar_acervo_solicitacao.cs
+++ SME.CDEP.TesteIntegracao/AcervoSolicitacao/Ao_cancelar_acervo_solicitacao.cs
@@ -12,8 +12,8 @@ namespace SME.CDEP.TesteIntegracao
         public Ao_cancelar_acervo_solicitacao(CollectionFixture collectionFixture) : base(collectionFixture)
         {}
        
-        [Fact(DisplayName = "Acervo Solicitação - Deve cancelar atendimento quando todos os itens estiverem cancelados")]
-        public async Task Deve_cancelar_atendimento_quando_todos_os_itens_estiverem_cancelados()
+        [Fact(DisplayName = "Acervo Solicitação - Deve cancelar atendimento quando itens aguardando visita")]
+        public async Task Deve_cancelar_atendimento_quando_itens_aguardando_visita()
         {
             await InserirDadosBasicosAleatorios();
 
@@ -28,7 +28,7 @@ namespace SME.CDEP.TesteIntegracao
            foreach (var item in acervoSolicitacao.Itens)
            {
                 item.AcervoSolicitacaoId = acervoSolicitadoId;
-                item.Situacao = SituacaoSolicitacaoItem.CANCELADO;
+                item.Situacao = SituacaoSolicitacaoItem.AGUARDANDO_VISITA;
                 await InserirNaBase(item);
            }
             
@@ -40,6 +40,109 @@ namespace SME.CDEP.TesteIntegracao
             var solicitacaoAlterada = ObterTodos<AcervoSolicitacao>().FirstOrDefault(f=> f.Id == 1);
             solicitacaoAlterada.Id.ShouldBe(1);
             solicitacaoAlterada.Situacao.ShouldBe(SituacaoSolicitacao.CANCELADO);
+
+            var itens = ObterTodos<AcervoSolicitacaoItem>();
+            itens.All(a=> a.Situacao.EstaCancelado()).ShouldBeTrue();
+        }
+        
+        [Fact(DisplayName = "Acervo Solicitação - Deve cancelar atendimento quando itens cancelados")]
+        public async Task Deve_cancelar_atendimento_quando_itens_cancelados()
+        {
+            await InserirDadosBasicosAleatorios();
+
+            await InserirAcervoTridimensional();
+
+            var acervoSolicitacao = ObterAcervoSolicitacao();
+            
+            await InserirNaBase(acervoSolicitacao);
+
+            var acervoSolicitadoId = (ObterTodos<AcervoSolicitacao>()).LastOrDefault().Id;
+           
+            foreach (var item in acervoSolicitacao.Itens)
+            {
+                item.AcervoSolicitacaoId = acervoSolicitadoId;
+                item.Situacao = SituacaoSolicitacaoItem.CANCELADO;
+                await InserirNaBase(item);
+            }
+            
+            var servicoAcervoSolicitacao = GetServicoAcervoSolicitacao();
+            
+            var retorno = await servicoAcervoSolicitacao.CancelarAtendimento(1);
+            
+            retorno.ShouldBeTrue();
+            var solicitacaoAlterada = ObterTodos<AcervoSolicitacao>().FirstOrDefault(f=> f.Id == 1);
+            solicitacaoAlterada.Id.ShouldBe(1);
+            solicitacaoAlterada.Situacao.ShouldBe(SituacaoSolicitacao.CANCELADO);
+            
+            var itens = ObterTodos<AcervoSolicitacaoItem>();
+            itens.All(a=> a.Situacao.EstaCancelado()).ShouldBeTrue();
+        }
+        
+        [Fact(DisplayName = "Acervo Solicitação - Deve cancelar atendimento quando itens aguardando atendimento")]
+        public async Task Deve_cancelar_atendimento_quando_itens_aguardando_atendimento()
+        {
+            await InserirDadosBasicosAleatorios();
+
+            await InserirAcervoTridimensional();
+
+            var acervoSolicitacao = ObterAcervoSolicitacao();
+            
+            await InserirNaBase(acervoSolicitacao);
+
+            var acervoSolicitadoId = (ObterTodos<AcervoSolicitacao>()).LastOrDefault().Id;
+           
+            foreach (var item in acervoSolicitacao.Itens)
+            {
+                item.AcervoSolicitacaoId = acervoSolicitadoId;
+                item.Situacao = SituacaoSolicitacaoItem.AGUARDANDO_ATENDIMENTO;
+                item.DataVisita = DateTimeExtension.HorarioBrasilia().AddDays(5);
+                await InserirNaBase(item);
+            }
+            
+            var servicoAcervoSolicitacao = GetServicoAcervoSolicitacao();
+            
+            var retorno = await servicoAcervoSolicitacao.CancelarAtendimento(1);
+            
+            retorno.ShouldBeTrue();
+            var solicitacaoAlterada = ObterTodos<AcervoSolicitacao>().FirstOrDefault(f=> f.Id == 1);
+            solicitacaoAlterada.Id.ShouldBe(1);
+            solicitacaoAlterada.Situacao.ShouldBe(SituacaoSolicitacao.CANCELADO);
+            
+            var itens = ObterTodos<AcervoSolicitacaoItem>();
+            itens.All(a=> a.Situacao.EstaCancelado()).ShouldBeTrue();
+        }
+        
+        [Fact(DisplayName = "Acervo Solicitação - Deve cancelar atendimento quando itens finalizados manualmente")]
+        public async Task Deve_cancelar_atendimento_quando_itens_finalizado_manualmente()
+        {
+            await InserirDadosBasicosAleatorios();
+
+            await InserirAcervoTridimensional();
+
+            var acervoSolicitacao = ObterAcervoSolicitacao();
+            
+            await InserirNaBase(acervoSolicitacao);
+
+            var acervoSolicitadoId = (ObterTodos<AcervoSolicitacao>()).LastOrDefault().Id;
+           
+            foreach (var item in acervoSolicitacao.Itens)
+            {
+                item.AcervoSolicitacaoId = acervoSolicitadoId;
+                item.Situacao = SituacaoSolicitacaoItem.FINALIZADO_MANUALMENTE;
+                await InserirNaBase(item);
+            }
+            
+            var servicoAcervoSolicitacao = GetServicoAcervoSolicitacao();
+            
+            var retorno = await servicoAcervoSolicitacao.CancelarAtendimento(1);
+            
+            retorno.ShouldBeTrue();
+            var solicitacaoAlterada = ObterTodos<AcervoSolicitacao>().FirstOrDefault(f=> f.Id == 1);
+            solicitacaoAlterada.Id.ShouldBe(1);
+            solicitacaoAlterada.Situacao.ShouldBe(SituacaoSolicitacao.CANCELADO);
+            
+            var itens = ObterTodos<AcervoSolicitacaoItem>();
+            itens.All(a=> a.Situacao.EstaCancelado()).ShouldBeTrue();
         }
         
         [Fact(DisplayName = "Acervo Solicitação - Não deve cancelar atendimento com itens em situação finalizado automaticamente")]
