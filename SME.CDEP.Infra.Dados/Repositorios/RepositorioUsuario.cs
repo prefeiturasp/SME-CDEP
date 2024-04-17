1103c273e646bd80bf8fa74e2883d685e9e6c827
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-04-17 09:30:01 -0300

    Correção do retorno do enum

--- SME.CDEP.Infra.Dados/Repositorios/RepositorioUsuario.cs
+++ SME.CDEP.Infra.Dados/Repositorios/RepositorioUsuario.cs
@@ -11,13 +11,33 @@ namespace SME.CDEP.Infra.Dados.Repositorios
         { }
 
         public Task<Usuario> ObterPorLogin(string login)
-            => conexao.Obter()
-                .QueryFirstOrDefaultAsync<Usuario>("select id, criado_em, criado_por, alterado_em, alterado_por, " +
-                                                      "criado_login, alterado_login, login, ultimo_login, nome, " +
-                                                      "expiracao_recuperacao_senha, token_recuperacao_senha, cep, " +
-                                                      "telefone, endereco, numero, complemento, cidade, estado, " +
-                                                      "bairro, tipo, email, instituicao " +
-                                                      "from usuario " +
-                                                      "where login = @login", new { login });
+        {
+            var query = @"select id, 
+                                 criado_em, 
+                                 criado_por, 
+                                 alterado_em, 
+                                 alterado_por, 
+                                 criado_login, 
+                                 alterado_login, 
+                                 login, 
+                                 ultimo_login, 
+                                 nome, 
+                                 expiracao_recuperacao_senha, 
+                                 token_recuperacao_senha, 
+                                 cep, 
+                                 telefone, 
+                                 endereco, 
+                                 numero, 
+                                 complemento, 
+                                 cidade, 
+                                 estado, 
+                                 bairro, 
+                                 tipo, 
+                                 email, 
+                                 instituicao 
+                          from usuario 
+                          where login = @login";
+            return conexao.Obter().QueryFirstOrDefaultAsync<Usuario>(query, new { login });
+        }
     }
 }
