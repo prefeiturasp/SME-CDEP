6de26722037f9129b27ff6ff21da8ac77e26137c
Vinícius Adelir Nyari <viniciusnyari@gmail.com>
2024-04-18 10:17:12 -0300

    Correção do script

--- scripts/V48__ALTER_AUMENTAR_CAMPO_NOME_ASSUNTO_PARA_270.sql
+++ scripts/V48__ALTER_AUMENTAR_CAMPO_NOME_ASSUNTO_PARA_270.sql
@@ -1,5 +1,5 @@
 --> Alterar tamanho do campo nome
-ALTER TABLE public.assunto ALTER COLUMN nome TYPE varchar(270)
+ALTER TABLE public.assunto ALTER COLUMN nome TYPE varchar(270);
 
 --> Alterar tamanho do campo tipo autoria
-ALTER TABLE public.acervo_credito_autor ALTER COLUMN tipo_autoria TYPE varchar(30)
+ALTER TABLE public.acervo_credito_autor ALTER COLUMN tipo_autoria TYPE varchar(30);
