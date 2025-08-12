db8b444cc9c7103753f00b6e1e7c6b6d98ad41d5
Diego Moreno <dierudito@gmail.com>
2025-08-12 17:15:48 -0300

    Update V54__ALTER_TABLE_ACERVO_DOCUMENTAL_CAPA_DOCUMENTO_OPCIONAL.sql

--- scripts/V54__ALTER_TABLE_ACERVO_DOCUMENTAL_CAPA_DOCUMENTO_OPCIONAL.sql
+++ scripts/V54__ALTER_TABLE_ACERVO_DOCUMENTAL_CAPA_DOCUMENTO_OPCIONAL.sql
@@ -1,3 +1,6 @@
+-- Remove a coluna capa_documento da tabela acervo se existir
+ALTER TABLE public.acervo DROP IF EXISTS capa_documento;
+
 -- Adiciona a nova coluna para armazenar o nome do arquivo da imagem da capa
 ALTER TABLE public.acervo_documental
 ADD COLUMN capa_documento VARCHAR(255) NULL;
