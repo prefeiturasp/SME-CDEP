71f74d410365feb8ba206301d5f99a3a93977bb6
Diego Moreno <dierudito@gmail.com>
2025-08-12 11:04:07 -0300

    Atualizar scripts de migração de colunas capa_documento

--- scripts/V53__ALTER_TABLE_ACERVO_CAPA_DOCUMENTO_OPCIONAL.sql
+++
@@ -1,6 +0,0 @@
--- Remoção dessa coluna pois não é responsabilidade dessa tabela
-ALTER TABLE public.acervo DROP COLUMN IF EXISTS capa_documento;
-
--- Adiciona a nova coluna para armazenar o nome do arquivo da imagem da capa
-ALTER TABLE public.acervo_documental
-ADD COLUMN capa_documento VARCHAR(255) NULL;
