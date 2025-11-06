e180301fc182a892af050441156d0704fcbbefdc
Diego Moreno <dierudito@gmail.com>
2025-11-06 08:08:55 -0300

    Update RegistradorDependencias.cs

--- teste/SME.CDEP.TesteIntegracao/Setup/RegistradorDependencias.cs
+++ teste/SME.CDEP.TesteIntegracao/Setup/RegistradorDependencias.cs
@@ -142,6 +142,7 @@ namespace SME.CDEP.TesteIntegracao.Setup
             _serviceCollection.TryAddScoped<IImportacaoArquivoAcervoDocumentalAuxiliar, ExecutarImportacaoArquivoAcervoDocumentalUseCase>();
             _serviceCollection.TryAddScoped<IRelatorioControleLivrosEmprestadosUseCase, RelatorioControleLivrosEmprestadosUseCase>();
             _serviceCollection.TryAddScoped<IServicoDeConsolidacao, ServicoDeConsolidacao>();
+            _serviceCollection.TryAddScoped<IServicoHistoricoConsultaAcervo, ServicoHistoricoConsultaAcervo>();
         }
         protected override void RegistrarHttpClients()
         {}
