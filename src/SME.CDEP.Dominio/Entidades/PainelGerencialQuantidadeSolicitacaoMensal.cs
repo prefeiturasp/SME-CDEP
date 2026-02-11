305c5f97b7dd8959e1b7587d661ee9dd97653c96
Diego Moreno <dierudito@gmail.com>
2026-02-11 10:09:22 -0300

    Altera endpoint de gráfico solicitações mensais
    AB#142647

--- src/SME.CDEP.Dominio/Entidades/PainelGerencialQuantidadeSolicitacaoMensal.cs
+++ src/SME.CDEP.Dominio/Entidades/PainelGerencialQuantidadeSolicitacaoMensal.cs
@@ -4,5 +4,7 @@
     {
         public DateOnly MesReferencia { get; set; }
         public int TotalSolicitacoes { get; set; }
+        public int TotalAutomatica { get; set; }
+        public int TotalManual { get; set; }
     }
 }
